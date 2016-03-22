a2beb5dd3bd496347466b87f6d69542a5f13f84e
Norton Pengra <npengra317@gmail.com>
2016-03-22 11:26:57 -0700

    Removed useless code

--- learning_journal/learning_journal/tests/test_models.py
+++ learning_journal/learning_journal/tests/test_models.py
@@ -3,12 +3,12 @@
 from learning_journal.models import Entry, DBSession
 
 
-def test_create_mymodel_entry(dbtransaction, dummy_request):
+def test_create_mymodel_entry(dbtransaction):
     """Test creation of model."""
     new_model = Entry(title="Norton", text="waffles")
     assert new_model.id is None
-    DBSession.add(new_model)
-    DBSession.flush()
+    dbtransaction.add(new_model)
+    dbtransaction.flush()
     assert new_model.id is not None
     assert new_model.text == 'waffles'
     assert new_model.title == 'Norton'
