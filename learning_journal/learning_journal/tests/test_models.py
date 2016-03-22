253da83fda8a93ec9ac86b8d300cc565b4c5f98c
Norton Pengra <npengra317@gmail.com>
2016-03-22 11:32:36 -0700

    Fixed attribute error in tests

--- learning_journal/learning_journal/tests/test_models.py
+++ learning_journal/learning_journal/tests/test_models.py
@@ -3,13 +3,16 @@
 from learning_journal.models import Entry, DBSession
 
 
-def test_create_mymodel_entry(dbtransaction):
+def test_create_mymodel_entry():
     """Test creation of model."""
     new_model = Entry(title="Norton", text="waffles")
     assert new_model.id is None
-    dbtransaction.add(new_model)
-    dbtransaction.flush()
+    DBSession.add(new_model)
+    DBSession.flush()
     assert new_model.id is not None
     assert new_model.text == 'waffles'
     assert new_model.title == 'Norton'
     assert new_model.created is not None
+
+def test_delete_mymodel_entry(loadeddb):
+    import pdb; pdb.set_trace()
