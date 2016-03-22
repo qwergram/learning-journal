468e1a0569d015fe9dc7599aa9d082d1ac09e673
Norton Pengra <npengra317@gmail.com>
2016-03-22 11:24:29 -0700

    Condensed Tests

--- learning_journal/learning_journal/tests/test_models.py
+++ learning_journal/learning_journal/tests/test_models.py
@@ -10,30 +10,6 @@ def test_create_mymodel_entry(dbtransaction, dummy_request):
     DBSession.add(new_model)
     DBSession.flush()
     assert new_model.id is not None
-
-
-def test_create_mymodel_text(dbtransaction, dummy_request):
-    """Test text creation of model."""
-    new_model = Entry(title="Norton", text="waffles")
-    assert new_model.id is None
-    DBSession.add(new_model)
-    DBSession.flush()
     assert new_model.text == 'waffles'
-
-
-def test_create_mymodel_title(dbtransaction, dummy_request):
-    """Test title creation of model."""
-    new_model = Entry(title="Norton", text="waffles")
-    assert new_model.id is None
-    DBSession.add(new_model)
-    DBSession.flush()
     assert new_model.title == 'Norton'
-
-
-def test_create_mymodel_created(dbtransaction, dummy_request):
-    """Test "created" creation of model."""
-    new_model = Entry(title="Norton", text="waffles")
-    assert new_model.id is None
-    DBSession.add(new_model)
-    DBSession.flush()
     assert new_model.created is not None
